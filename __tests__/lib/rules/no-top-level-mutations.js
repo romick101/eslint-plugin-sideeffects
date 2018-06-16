6a971c51622d9881e206562388e6667f8d68c244
romick101 <romick101@gmail.com>
2018-06-17 01:28:48 +0300

    initial no-top-level-mutations

--- __tests__/lib/rules/no-top-level-mutations.js
+++ __tests__/lib/rules/no-top-level-mutations.js
@@ -1,21 +1,54 @@
 'use strict';
 
 const rule = require('../../../lib/rules/no-top-level-mutations'),
-    RuleTester = require('eslint').RuleTester;
+  RuleTester = require('eslint').RuleTester;
 
 RuleTester.setDefaultConfig({
-    parserOptions: {
-        ecmaVersion: 6
-    }
+  parserOptions: {
+    ecmaVersion: 6
+  }
 });
 const ruleTester = new RuleTester();
 
 ruleTester.run('no-top-level-mutations', rule, {
-    valid: [{
-        code: ``
-    }],
-    invalid: [{
-        code: ``,
-        errors: []
+  valid: [{
+    code: `
+          //   function foo() {
+          //   var localVariable = {
+          //     key: 'DefaultValue'};
+          //   localVariable.key = 'AlteredValue';
+          //   if (true) {
+          //     let ScopedVariable = [1, 2, 3];
+          //     ScopedVariable.push(4);
+          //   }
+          //   const closure = {key: 'EnclosedValue'};
+          //   const bar = () => 
+          //     Object.assign(closure, {
+          //       key: 'AlteredEnclosedValue'});
+          // }
+          `
+  }],
+  invalid: [{
+    code: `
+          const explicitGlobal = {
+            key: 'DefaultValue'
+          };
+          if (true) {
+            implicitGlobal = [1, 2, 3];
+          }
+          function foo() {
+            // Object.assign(explicitGlobal, {
+            //   key: 'AlteredValue'});
+            implicitGlobal.sort();
+           // process.env.HEAP_SIZE = null;
+          }
+          `,
+    errors: [{
+      message: 'Mutating global variable explicitGlobal'
+    }, {
+      message: 'Mutating global variable implicitGlobal'
+    }, {
+      message: 'Mutating global variable process'
     }]
+  }]
 });
